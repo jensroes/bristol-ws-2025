9d3bb11689606d0571f198de74d174d746464451
jensroes <jens.roes@mail.com>
2025-03-20 20:23:02 +0000

    First final version of linear models workshops

--- linear-models/exercises/normal_model_simulation.R
+++ linear-models/exercises/normal_model_simulation.R
@@ -21,25 +21,31 @@ sim_data <- tibble(group_1 = rnorm(n = n/2,
                                   mean = beta_0 + beta_1,
                                   sd = ---)) # needs to be the standard deviation
 
-# Preview data
+# Important for the simulated data is to note that one group includes beta_1
+# and the other doesn't.
+
+# Preview data by adding in the same of the simulate data frame
 glimpse(---)
 
-# Transform data
+# Transform data from wide to long format
 sim_data <- pivot_longer(sim_data,
                          cols = c(group_1, group_2),
                          names_to = "x",
                          values_to = "y")
 
-# Preview data
+# Preview data the long-format data
 glimpse(---)
 # Make sure you understand how the transformation affected the data frame.
 
-# Normal linear model of simulated data
+# Normal linear model of simulated data: include the outcome variable on the left of "~"
+# and the categorical predictor variable on the right of "~".
 model <- lm(--- ~ ---, data = sim_data)
 
-# Model coefficients
+# Model coefficients: add the name of the model
 coef(---)
 
-# Standard deviation
+# Standard deviation: add the name of the model
 sigma(---)
 
+# Task: check if the parameter estimates of the model match the parameters
+# you used for the simulation.
